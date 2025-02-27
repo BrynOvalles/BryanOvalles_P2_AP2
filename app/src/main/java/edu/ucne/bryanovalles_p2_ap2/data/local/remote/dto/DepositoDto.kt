e9c6a12c7a0ff53f0edceb4185b1da3bef5c84a8
BrynOvalles <brynovallesburgos@gmail.com>
2025-02-26 20:34:09 -0400

    Creando Dto de Deposito

---
+++ app/src/main/java/edu/ucne/bryanovalles_p2_ap2/data/local/remote/dto/DepositoDto.kt
@@ -0,0 +1,9 @@
+package edu.ucne.bryanovalles_p2_ap2.data.local.remote.dto
+
+class DepositoDto (
+    val idDeposito: Int?,
+    val fecha: String,
+    val idCuenta: Int?,
+    val concepto: String,
+    val monto: Int?
+)
