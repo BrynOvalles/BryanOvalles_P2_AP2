1ac7f9c5f79b3c88ca93352ee64305e543d33ac4
BrynOvalles <brynovallesburgos@gmail.com>
2025-02-26 20:33:57 -0400

    Creando Dao

---
+++ app/src/main/java/edu/ucne/bryanovalles_p2_ap2/data/local/dao/DepositoDao.kt
@@ -0,0 +1,30 @@
+package edu.ucne.bryanovalles_p2_ap2.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import edu.ucne.bryanovalles_p2_ap2.data.local.entity.DepositoEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface DepositoDao {
+    @Upsert()
+    suspend fun save(depositoEntity: DepositoEntity)
+
+    @Query(
+        """
+            SELECT *
+            FROM "deposito"
+            WHERE idDeposito=:id
+            LIMIT 1
+        """
+    )
+    suspend fun find(id: Int): DepositoEntity?
+
+    @Delete
+    suspend fun delete(depositoEntity: DepositoEntity)
+
+    @Query("SELECT * FROM Deposito")
+    fun getAll(): Flow<List<DepositoEntity>>
+}
