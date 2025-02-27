308923be40e4d842b0e2a03314a5ae0f6ea6ca10
BrynOvalles <brynovallesburgos@gmail.com>
2025-02-26 20:36:24 -0400

    Agregando Resource y el RemoteDataSource

---
+++ app/src/main/java/edu/ucne/bryanovalles_p2_ap2/data/local/remote/RemoteDataSource.kt
@@ -0,0 +1,14 @@
+package edu.ucne.bryanovalles_p2_ap2.data.local.remote
+
+import edu.ucne.bryanovalles_p2_ap2.data.local.remote.dto.DepositoDto
+import javax.inject.Inject
+
+class RemoteDataSource @Inject constructor(
+    private val depositoApi: DepositoApi
+) {
+    suspend fun getDepositos() = depositoApi.getDeposito()
+    suspend fun getDeposito(id: Int) = depositoApi.getDeposito(id)
+    suspend fun saveDeposito(depositoDto: DepositoDto) = depositoApi.saveDeposito(depositoDto)
+    suspend fun updateDeposito(id: Int, depositoDto: DepositoDto) = depositoApi.updateDeposito(id, depositoDto)
+    suspend fun deleteDeposito(id: Int) = depositoApi.deleteDeposito(id)
+}
