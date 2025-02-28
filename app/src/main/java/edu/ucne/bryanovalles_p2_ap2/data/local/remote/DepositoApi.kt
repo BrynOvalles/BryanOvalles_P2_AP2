026831d9fbb1dfe39b59d419d45e7b6826339405
BrynOvalles <brynovallesburgos@gmail.com>
2025-02-28 08:38:02 -0400

    Implementando Retrofit en DepositoApi para manejo de headers, GET, POST, PUT, y DELETE.

--- app/src/main/java/edu/ucne/bryanovalles_p2_ap2/data/local/remote/DepositoApi.kt
+++ app/src/main/java/edu/ucne/bryanovalles_p2_ap2/data/local/remote/DepositoApi.kt
@@ -5,26 +5,32 @@ import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.Path
 import retrofit2.http.POST
 import retrofit2.http.PUT
 
 interface DepositoApi {
+    @Headers("X-API-Key:test")
     @GET("api/Depositos")
-    suspend fun getDeposito(): List<DepositoDto>
+    suspend fun getDepositos(): List<DepositoDto>
 
+    @Headers("X-API-Key:test")
     @GET("api/Depositos/{id}")
     suspend fun getDeposito(@Path("id") id: Int): DepositoDto
 
+    @Headers("X-API-Key:test")
     @POST("api/Depositos")
-    suspend fun saveDeposito(@Body entidadDto: DepositoDto?): DepositoDto
+    suspend fun saveDeposito(@Body entidadDto: DepositoDto?): Response<DepositoDto>
 
+    @Headers("X-API-Key:test")
     @PUT("api/Depositos/{id}")
     suspend fun updateDeposito(
         @Path("id") id: Int,
         @Body entidad: DepositoDto
     ): Response<DepositoDto>
 
+    @Headers("X-API-Key:test")
     @DELETE("api/Depositos/{id}")
     suspend fun deleteDeposito(@Path("id") id: Int): Response<Unit>
 }
