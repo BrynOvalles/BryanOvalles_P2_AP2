3983f62810080da15863dbdd2dea3ec67d372615
BrynOvalles <brynovallesburgos@gmail.com>
2025-02-28 08:45:15 -0400

    renombrando getDeposito a getDepositos en el RemoteDataSource

--- app/src/main/java/edu/ucne/bryanovalles_p2_ap2/data/local/remote/RemoteDataSource.kt
+++ app/src/main/java/edu/ucne/bryanovalles_p2_ap2/data/local/remote/RemoteDataSource.kt
@@ -6,7 +6,7 @@ import javax.inject.Inject
 class RemoteDataSource @Inject constructor(
     private val depositoApi: DepositoApi
 ) {
-    suspend fun getDepositos() = depositoApi.getDeposito()
+    suspend fun getDepositos() = depositoApi.getDepositos()
     suspend fun getDeposito(id: Int) = depositoApi.getDeposito(id)
     suspend fun saveDeposito(depositoDto: DepositoDto) = depositoApi.saveDeposito(depositoDto)
     suspend fun updateDeposito(id: Int, depositoDto: DepositoDto) = depositoApi.updateDeposito(id, depositoDto)
