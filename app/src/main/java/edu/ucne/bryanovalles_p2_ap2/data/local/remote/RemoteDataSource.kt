e4465e74cec84f47e32eb00d0f735975aecf92d4
BrynOvalles <brynovallesburgos@gmail.com>
2025-02-28 21:42:18 -0400

    removiendo desde el DataSource el metodo inusado en la api

--- app/src/main/java/edu/ucne/bryanovalles_p2_ap2/data/local/remote/RemoteDataSource.kt
+++ app/src/main/java/edu/ucne/bryanovalles_p2_ap2/data/local/remote/RemoteDataSource.kt
@@ -7,7 +7,6 @@ class RemoteDataSource @Inject constructor(
     private val depositoApi: DepositoApi
 ) {
     suspend fun getDepositos() = depositoApi.getDepositos()
-    suspend fun getDeposito(id: Int) = depositoApi.getDeposito(id)
     suspend fun saveDeposito(depositoDto: DepositoDto) = depositoApi.saveDeposito(depositoDto)
     suspend fun updateDeposito(id: Int, depositoDto: DepositoDto) = depositoApi.updateDeposito(id, depositoDto)
     suspend fun deleteDeposito(id: Int) = depositoApi.deleteDeposito(id)
